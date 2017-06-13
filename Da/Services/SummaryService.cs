fb0fd0015123b43f25b8e2540c1ddca3c14af1d4
Grzegorz <g.mrukwa@gmail.com>
2017-06-13 19:24:59 +0200

    Fixed summary of payoffs

--- Da/Services/SummaryService.cs
+++ Da/Services/SummaryService.cs
@@ -8,14 +8,21 @@ namespace Da.Services
     {
         public void CountPayoffs()
         {
-            double sum, min, max;
+            double sum = 0;
+            double min = 0;
+            double max = 0;
             using (var context = new Context())
             {
-                sum = context.Salaries.Sum(s => s.Amount);
-                min = context.Salaries.Min(s => s.Amount);
-                max = context.Salaries.Max(s => s.Amount);
+                if (context.Salaries.Any())
+                {
+                    sum = context.Salaries.Sum(s => s.Amount);
+                    min = context.Salaries.Min(s => s.Amount);
+                    max = context.Salaries.Max(s => s.Amount);
+                }
             }
             MessageBox.Show("Sum: " + sum + "\nMin: " + min + "\nMax: " + max, "Payoffs summary", MessageBoxButton.OK);
         }
+
+
     }
 }
