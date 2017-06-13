2839446f658d00c5c4c40f439245376dea054b45
Grzegorz <g.mrukwa@gmail.com>
2017-06-13 16:25:00 +0200

    Add payoff summary

---
+++ Da/Services/SummaryService.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Windows;
+using Backend.Utils;
+
+namespace Da.Services
+{
+    class SummaryService
+    {
+        public void CountPayoffs()
+        {
+            double sum, min, max;
+            using (var context = new Context())
+            {
+                sum = context.Salaries.Sum(s => s.Amount);
+                min = context.Salaries.Min(s => s.Amount);
+                max = context.Salaries.Max(s => s.Amount);
+            }
+            MessageBox.Show("Sum: " + sum + "\nMin: " + min + "\nMax: " + max, "Payoffs summary", MessageBoxButton.OK);
+        }
+    }
+}
